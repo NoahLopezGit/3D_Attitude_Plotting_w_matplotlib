1366be25511b5a84d2438544a2791f725e9d8a8b
Noah Lopez <loalno12@gmail.com>
2021-11-07 14:02:28 -0600

    added new disturbances and fixed issues

--- Disturbance_Torques.py
+++ Disturbance_Torques.py
@@ -6,8 +6,8 @@ import csv
 
 class Orientation:
     def __init__(self,  date            = datetime.datetime(1,1,1,1,1,1),
-                        position        = np.matrix([ [0], [0], [0] ]), 
-                        velocity        = np.matrix([ [0], [0], [0] ]), 
+                        position        = np.matrix([ [0], [0], [0] ]).astype(float), 
+                        velocity        = np.matrix([ [0], [0], [0] ]).astype(float), 
                         euler_angles    = np.array([0,0,0])):
         self.date           = date
         self.position       = position
@@ -16,13 +16,21 @@ class Orientation:
 
 class Plate:
     def __init__(self,  area            = 0.0,
-                        normal          = np.matrix([ [1], [0], [0] ]), 
-                        lever           = np.matrix([ [1], [0], [0] ]) , 
+                        normal          = np.matrix([ [1], [0], [0] ]).astype(float), 
+                        lever           = np.matrix([ [1], [0], [0] ]).astype(float) , 
                         r_coef          = np.array([0,0,0])):
         self.area   = area
         self.normal = normal
         self.lever  = lever
         self.r_coef = r_coef
+
+
+def sind(deg):
+    return np.sin(np.deg2rad(deg))
+
+
+def cosd(deg):
+    return np.cos(np.deg2rad(deg))
 
 
 #importing atmospheric data from csv
@@ -35,16 +43,16 @@ with open('Atmospheric_density_model.csv', 'r', newline='') as csvfile:
 
 #constants
 PHI = 1361 #solar constant TODO units
-c = 3*10**8 #speed of light (m/s)
-Wo = np.matrix([ [0], [0], [0.000072921158553]]) #earths rotation in rads/s
-Cd = 2.25 #typical coefficient of drag ~2-2.5
-
-def sind(deg):
-    return np.sin(np.deg2rad(deg))
-
-
-def cosd(deg):
-    return np.cos(np.deg2rad(deg))
+c   = 3*10**8 #speed of light (m/s)
+Wo  = np.matrix([ [0], [0], [0.000072921158553]]).astype(float) #earths rotation in rads/s
+Cd  = 2.25 #typical coefficient of drag ~2-2.5
+Me  = 5.97219*10**24 #earth mass
+Gc  = 6.67408*10**-11 #gravitational constant
+u   = Me * Gc #earth gravitational constant
+#I THINK THIS MUST BE CONVERTED FROM NANO TO REG SI SO 10E-9 TODO: FIGURE OUT IF THIS IS CORRECT
+m_vec = 7.77*10**(21.0-9.0)*np.matrix([   [sind(169.7)*cosd(108.2)], 
+                                    [sind(169.7)*sind(108.2)], 
+                                    [cosd(169.7)            ] ]).astype(float)
 
 
 #gives sattelite-sun vector in ECI frame
@@ -55,11 +63,11 @@ def get_rsato(date,r_sattoearth):
     Phi_o   = 280.46 + 36000.771*Tut1
     Phi_ecl = Phi_o + 1.914666471*sind(Mo) + 0.019994643*sind(2*Mo)
 
-    roo_unitvec = np.matrix([ [cosd(Phi_ecl)], [cosd(Ep)*sind(Phi_ecl)], [sind(Ep)*sind(Phi_ecl)] ])
-    roo_mag = 1.000140612 - 0.016708617*cosd(Mo) - 0.000139589*cosd(2*Mo)
+    roo_unitvec = np.matrix([ [cosd(Phi_ecl)], [cosd(Ep)*sind(Phi_ecl)], [sind(Ep)*sind(Phi_ecl)] ]).astype(float)
+    roo_mag     = 1.000140612 - 0.016708617*cosd(Mo) - 0.000139589*cosd(2*Mo)
 
-    r_sattoearth = np.multiply(1/(1.496*10**8), r_sattoearth) #r_sattoearth given in km converted to AU
-    r_sato = np.multiply(roo_mag, roo_unitvec) - r_sattoearth
+    r_sattoearth    = np.multiply(1/(1.496*10**8), r_sattoearth) #r_sattoearth given in km converted to AU
+    r_sato          = np.multiply(roo_mag, roo_unitvec) - r_sattoearth
 
     return r_sato
 
@@ -75,24 +83,24 @@ def get_dcm(angle_vec, order_vec):
         [ np.cos(ang[0]), np.sin(ang[0]), 0 ],
         [-np.sin(ang[0]), np.cos(ang[0]), 0 ],
         [ 0             , 0             , 1 ]
-    ])
+    ]).astype(float)
     y_DCM = np.matrix([
         [ np.cos(ang[1]), 0,-np.sin(ang[1]) ],
         [ 0             , 1, 0              ],
         [ np.sin(ang[1]), 0, np.cos(ang[1]) ]
-    ])
+    ]).astype(float)
     x_DCM = np.matrix([
         [  1, 0             , 0             ],
         [  0, np.cos(ang[2]), np.sin(ang[2])],
         [  0,-np.sin(ang[2]), np.cos(ang[2])]
-    ])
+    ]).astype(float)
     
     DCM_array = np.array([ x_DCM, y_DCM, z_DCM ]) # x==1, y==2, z==3 for purpose of DCM description
     DCM = np.matrix([
         [ 1, 0, 0],
         [ 0, 1, 0],
         [ 0, 0, 1]
-    ])
+    ]).astype(float)
     for i in range(3):
         DCM = np.matmul( DCM_array[order_vec[i]-1], DCM)
 
@@ -103,122 +111,189 @@ def get_dcm(angle_vec, order_vec):
 def get_Aio(position, velocity): 
     Aio = np.matrix([
         [0,0,0],
-        [0,0,0], 
+        [0,0,0],
         [0,0,0]
-    ])
-    Aio[:,0] = np.multiply( -position, 1/np.linalg.norm(position)) 
-    h = np.cross(position.transpose(), velocity.transpose())
-    Aio[:,1] = np.multiply( -h, 1/np.linalg.norm(h)).transpose()
-    Aio[:,2] = np.cross( Aio[:,1].transpose(), Aio[:,0].transpose() ).transpose()
+    ]).astype(float)
 
+    Aio[:,0]    = -1/np.linalg.norm(position)*position
+    h           = np.cross(position.transpose(), velocity.transpose())
+    Aio[:,1]    = np.multiply( -h, 1/np.linalg.norm(h)).transpose()
+    Aio[:,2]    = np.cross( Aio[:,1].transpose(), Aio[:,0].transpose() ).transpose()
     return Aio
 
 
 def get_density(alt):
 
+    #indexing atmos table to find correct altitude interval
     i=0
-    while alt >= atmos_table[i,1]:
+    while alt >= atmos_table[i][0]:
         i+=1
     
-    h0 = atmos_table[i,0]
-    H = atmos_table[i,2]
-    rho_0 = atmos_table[i,1]
-
-    rho = rho_0*np.exp(-(alt-h0)/H)
+    h0      = atmos_table[i-1][0]
+    rho_0   = atmos_table[i-1][1]
+    H       = atmos_table[i-1][2]
+    
+    #calculating density from exponential model
+    rho     = rho_0*np.exp(-(alt-h0)/H)
     return rho
 
 #calculation of solar torque on plate in body frame
-def get_solartorque(sat_orientation, plate):
+def get_solartorque(sat_orientation, plate_array):
     #transformation Abi
-    angles = sat_orientation.euler_angles
-    angles = np.multiply(np.pi/180, angles)
-    order = np.array([3,2,1])
-    Abo = get_dcm(angles, order)
-    Aoi = get_Aio(sat_orientation.position, sat_orientation.velocity).transpose()
-    Abi = np.matmul( Abo, Aoi)
-    
-    #sun-sat vector in body frame
-    r_sato = get_rsato(sat_orientation.date, sat_orientation.position)
-    s = np.matmul( Abi, r_sato)
+    Abo     = get_dcm(sat_orientation.euler_angles, np.array([3,2,1]))
+    Aoi     = get_Aio(sat_orientation.position, sat_orientation.velocity).transpose()
+    Abi     = np.matmul( Abo, Aoi)
 
-    #incidence angle of sun-sat vector and plate normal
-    cos_theta_srp = np.matmul( plate.normal.transpose(), s )[0,0]
+    #calculation
+    L_srp_total = np.matrix([ [0],[0],[0] ]).astype(float)
+    for plate in plate_array:
+        
+        #sun-sat vector in body frame
+        r_sato  = get_rsato(sat_orientation.date, sat_orientation.position)
+        s       = np.matmul( Abi, r_sato)
 
-    #calculation of solar pressure force of plate (in body frame)
-    r_diff = plate.r_coef[0]#note order
-    r_spec = plate.r_coef[1]
-    F_srp = -PHI/(c*np.linalg.norm(r_sato))*plate.area*( 2*(r_diff/3+r_spec*cos_theta_srp) + (1-r_spec)*s )*max(cos_theta_srp,0)
+        #incidence angle of sun-sat vector and plate normal
+        cos_theta_srp = np.matmul( plate.normal.transpose(), s )[0,0]
 
-    #calculation of solar presure torque of plate
-    L_srp = np.cross( plate.lever.transpose(), F_srp.transpose() ).transpose()
+        #calculation of solar pressure force of plate (in body frame)
+        r_diff  = plate.r_coef[0]#note order
+        r_spec  = plate.r_coef[1]
+        F_srp   = -PHI/(c*np.linalg.norm(r_sato))*plate.area*( 2*(r_diff/3+r_spec*cos_theta_srp) + (1-r_spec)*s )*max(cos_theta_srp,0)
 
-    return L_srp
+        #calculation of solar presure torque of plate
+        L_srp = np.cross( plate.lever.transpose(), F_srp.transpose() ).transpose()
+        L_srp_total = L_srp_total + L_srp
+    return L_srp_total
 
 
-def get_aerotorque(sat_orientation, plate):
+def get_aerotorque(sat_orientation, plate_array):
     #getting body orientation w.r.t. ECI
     Aoi = get_Aio(sat_orientation.position, sat_orientation.velocity).transpose()
     Abo = get_dcm(sat_orientation.euler_angles, np.array([3,2,1]))
     Abi = np.matmul(Abo, Aoi)
+
+    #calculation
+    L_aero_total = np.matrix([ [0],[0],[0] ]).astype(float)
+    for plate in plate_array:
+        #calculate Vrel in body
+        v_reli = sat_orientation.position + np.matrix(np.cross( Wo.transpose(), 
+                                                                sat_orientation.position.transpose() )).astype(float).transpose()
+        v_relb = np.matmul( Abi, v_reli )
+
+        #calculation force on plate due to aerodynamic pressure F_aero
+        cos_theta_aero  = 1/np.linalg.norm(v_relb)*np.matmul( plate.normal.transpose(), v_relb)[0,0]
+        alt             = np.linalg.norm(sat_orientation.position) - 6371.0
+        rho             = get_density(alt)
+        F_aero          = -0.5*rho*Cd*np.linalg.norm(v_relb)*plate.area*max(cos_theta_aero,0)*v_relb
+
+        #calculation of torque on plate due to aerodynamic pressure L_aero
+        L_aero          = np.cross( plate.lever.transpose(), F_aero.transpose() ).transpose()
+        L_aero_total    = L_aero_total + L_aero   
+    return L_aero_total
+
+#calculated gravity torque from position, orientation and principle axis MOIs
+def get_gravitytorque(sat_orientation, Jp): 
+    psi     = sat_orientation.euler_angles[0]
+    theta   = sat_orientation.euler_angles[1]
+    Rc      = np.linalg.norm(sat_orientation.position)*10**3.0
+    vec     = np.matrix([
+        [(Jp[2]-Jp[1])*cosd(theta)**2.0*cosd(psi)*sind(psi) ],
+        [(Jp[2]-Jp[0])*cosd(theta)*sind(theta)*cosd(psi)    ],
+        [(Jp[0]-Jp[1])*cosd(theta)*sind(theta)*sind(psi)    ]
+    ]).astype(float)
+    L_grav  = 3*u/Rc**3.0*vec
+    return L_grav
+
+
+def get_magnetictorque(sat_orientation, magnetic_residual):
+    r = 10**3.0*sat_orientation.position
+    Bi = ( 3*np.matmul(m_vec.transpose(), r)[0,0]*r - np.linalg.norm(r)**2.0*m_vec) / np.linalg.norm(r)**5.0
     
-    #calculate Vrel in body
-    v_reli = sat_orientation.position + np.cross( Wo.transpose(), sat_orientation.position.transpose() )
-    v_relb = np.matmul( Abi, v_reli ) 
+    #getting body orientation w.r.t. ECI
+    Aoi = get_Aio(sat_orientation.position, sat_orientation.velocity).transpose()
+    Abo = get_dcm(sat_orientation.euler_angles, np.array([3,2,1]))
+    Abi = np.matmul(Abo, Aoi)
 
-    #calculation force on plate due to aerodynamic pressure F_aero
-    cos_theta_aero = 1/np.linalg.norm(v_relb)*np.matmul( plate.normal.tranpose(), v_relb)
-    alt = np.linalg.norm(sat_orientation.position) - 6371.0
-    rho = get_density(alt)
-    F_aero = -0.5*rho*Cd*np.linalg.norm(v_relb)*plate.area*max(cos_theta_aero,0)*v_relb
-
-    #calculation of torque on plate due to aerodynamic pressure L_aero
-    L_aero = np.cross( sat_orientation.lever.transpose(), F_aero.transpose() ).transpose()
-    return L_aero
-
+    #calculation of magnetic torque
+    L_magnetic = np.cross( magnetic_residual.transpose(), np.matmul(Abi, Bi).transpose() ).transpose()
+    return L_magnetic
 
 
 if __name__=="__main__":
 
-    date = datetime.datetime(2021,11,3,10,9,0)
-    r_sat = np.matrix([ [6971], [0], [0]])
-
-    r_sato = get_rsato(date,r_sat)
-    r_sato = np.multiply( 1/np.linalg.norm(r_sato), r_sato)
-
-    angles = np.array([45,45,45])
-    angles = np.multiply(np.pi/180, angles)
-    order = np.array([3,2,1])
-    Abo = get_dcm(angles, order)
-
+    #calculating satellite configuration values
+    #orientation
+    date    = datetime.datetime(2021,11,3,10,9,0)
+    angles  = np.array([30,45,10]) #3-2-1 angles in order which they are applied 
+    angles  = np.multiply(np.pi/180, angles)
     position = np.matrix([
-        [6971],
+        [6771],
         [0],
         [0]
-    ])
+    ]).astype(float)
+    v_mag = np.sqrt(u/(np.linalg.norm(position)*10**3.0)) / (10**3.0)
     velocity = np.matrix([
         [0],
-        [7.5615],
+        [v_mag],
         [0],
-    ])
-    Aoi = get_Aio(position, velocity).transpose()
-    Abi = np.matmul( Abo, Aoi)
-    print(Abi)
+    ]).astype(float)
 
-    r_sato_b = np.matmul(Abi, r_sato)
-    print(r_sato_b)
-    
     sat_orientation = Orientation() #this will get initialized with default values... must change to ones you want
     sat_orientation.date = date
     sat_orientation.position = position #describes position relative to center of earth in ECI frame
     sat_orientation.velocity = velocity  #these are in km and km/s units 
     sat_orientation.euler_angles = angles #in degrees
-    print(sat_orientation.date)
-    print(sat_orientation.euler_angles)
 
-    plate = Plate()
-    plate.area = 1
-    plate.lever = 1/np.sqrt(3)*np.matrix([ [1],[1],[1] ])
-    plate.normal = 1/np.sqrt(3)*np.matrix([ [1],[1],[1] ])
-    plate.r_coef = np.array([ 1/3, 1/3, 1/3 ])
+    #constructing plate(s) attributes
+    #doing a cube
+    bot         = Plate()
+    bot.area    = 1.0
+    bot.lever   = np.matrix([ [0],[0],[-0.5] ]).astype(float)
+    bot.normal  = np.matrix([ [0],[0],[-1.0] ]).astype(float)
+    bot.r_coef  = np.array([ 1/3, 1/3, 1/3 ])
 
-    print(get_solartorque(sat_orientation,plate)) #I think this works
+    top         = Plate()
+    top.area    = 1.0
+    top.lever   = np.matrix([ [0],[0],[0.5] ]).astype(float)
+    top.normal  = np.matrix([ [0],[0],[1.0] ]).astype(float)
+    top.r_coef  = np.array([ 1/3, 1/3, 1/3 ])
+    
+    north           = Plate()
+    north.area      = 1.0
+    north.lever     = np.matrix([ [0],[0.5],[0] ]).astype(float)
+    north.normal    = np.matrix([ [0],[1.0],[0] ]).astype(float)
+    north.r_coef    = np.array([ 1/3, 1/3, 1/3 ])
+
+    south           = Plate()
+    south.area      = 1.0
+    south.lever     = np.matrix([ [0],[-0.5],[0] ]).astype(float)
+    south.normal    = np.matrix([ [0],[-1.0],[0] ]).astype(float)
+    south.r_coef    = np.array([ 1/3, 1/3, 1/3 ])
+
+    west        = Plate()
+    west.area   = 1.0
+    west.lever  = np.matrix([ [-0.5],[0],[0] ]).astype(float)
+    west.normal = np.matrix([ [-1.0],[0],[0] ]).astype(float)
+    west.r_coef = np.array([ 1/3, 1/3, 1/3 ])
+
+    east        = Plate()
+    east.area   = 1.0
+    east.lever  = np.matrix([ [0.5],[0],[0] ]).astype(float)
+    east.normal = np.matrix([ [1.0],[0],[0] ]).astype(float)
+    east.r_coef = np.array([ 1/3, 1/3, 1/3 ])
+
+    plate_array = [bot, top, south, west, east, north]
+
+    print("Torque due to solar pressure on 1m^2 face cube")
+    print(get_solartorque(sat_orientation,plate_array)) #I think this works
+
+    print("Torque due to aerodynamic forces on 1m^2 face cube")
+    print(get_aerotorque(sat_orientation,plate_array)) #seems right
+
+    print("Torque due to gravity gradient")
+    principle_moments = np.array([ 25000, 25000, 77217 ]) #must define principle moment of intertia for satellite
+    print(get_gravitytorque(sat_orientation,principle_moments)) #seems rights
+
+    print("Torque due to magnetic interation")
+    mag_dipole = np.matrix([ [20], [20], [20] ]).astype(float) #must define a residual magnetic dipole for the satellite
+    print(get_magnetictorque(sat_orientation, mag_dipole))
